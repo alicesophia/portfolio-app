1ae2620ead327621b23584c501ffc187c38796a0
Alice Sophia Koch <alice@li-797500cc-2cc3-11b2-a85c-85fdab18fb90.ibm.com>
2022-08-20 13:29:27 -0300

    first commit correction

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {displayBio: false};
+  }
+
+  readMore() {
+    console.log("readMode this", this)
+    // this.setState({displayBio: true});
+  }
+
+    render() {
+        return (
+            <div>
+              <h1>Hello!</h1>
+              <p>My name is Alice. I'm a software engineer.</p>
+              <p>I'm always looking forward to learning interesting and different technologies.</p>
+              {
+                this.state.displayBio ? (
+                  <div>
+                    <p>I live in São Paulo, and code every day.</p>
+                    <p>My favorite language is Python, and I love .NET and JavaScript too :)</p>
+                    <p>Besides coding, I also love games and anime. My favourite game is Final Fantasy, and my favourite anime is One Piece.</p>
+                  </div>
+                ) : (
+                  <div>
+                    <button onClick={this.readMore}>Read more</button>
+                  </div>
+                )
+              }
+            </div>
+        )
+    }
+}
+
+export default App;
