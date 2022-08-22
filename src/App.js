b728ec779c4a11b712a7ea0a5956ae721a0fc083
Alice Sophia Koch <alice@li-797500cc-2cc3-11b2-a85c-85fdab18fb90.ibm.com>
2022-08-22 11:25:53 -0300

    Created projects component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Projects from "./Projects";
 
 class App extends Component {
 
@@ -28,6 +29,8 @@ class App extends Component {
             </div>
           )
         }
+        <hr/>
+        <Projects/>
       </div>
     )
   }
---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+import project4 from "../assets/project3.png";
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: "React application",
+    description: "A React app that I built, involving JS and core Web development concepts",
+    link: "https://github.com/alicesophia/portfolio-app",
+    image: project1
+  },
+  {
+    id: 2,
+    title: "SalesWeb MVC",
+    description: "A complete ASP.NET web page using MVC, Entity Framework and MySQL",
+    link: "https://github.com/alicesophia/salesweb-mvc",
+    image: project2
+  },
+  {
+    id: 3,
+    title: "Chess game",
+    description: "A Chess game made with C# using the core principles of POO",
+    link: "https://github.com/alicesophia/xadrez-console",
+    image: project3
+  },
+  {
+    id: 4,
+    title: "Others",
+    description: "See all the other cool projects I've built",
+    link: "https://github.com/alicesophia",
+    image: project4
+  }
+];
+
+export default PROJECTS;
