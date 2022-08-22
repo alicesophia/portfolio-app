a6f68c1f8e3db3c7c77d1145f59d08268f0ebe6d
alice <alicesophiakoch@gmail.com>
2022-08-22 16:10:21 -0300

    Added social profiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
 
 class App extends Component {
 
@@ -31,6 +32,8 @@ class App extends Component {
         }
         <hr/>
         <Projects/>
+        <hr/>
+        <SocialProfiles/>
       </div>
     )
   }
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import emailIcon from "../assets/email_icon.png";
+import githubIcon from "../assets/github_icon.png";
+import linkedinIcon from "../assets/linkedin_icon.png";
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: "mailto:alicesophiakoch@gmail.com",
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: "https://github.com/alicesophia",
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: "https://www.linkedin.com/in/alice-sophia-koch-25587a218/",
+    image: linkedinIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
