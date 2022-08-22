8a815094f042fc63bed912df63cb0381ef9454d3
Alice Sophia Koch <alice@li-797500cc-2cc3-11b2-a85c-85fdab18fb90.ibm.com>
2022-08-22 00:18:46 -0300

    State e setState

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import React, {Component} from "react";
 
 class App extends Component {
+
   constructor() {
     super();
     this.state = {displayBio: false};
+
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
   }
 
-  readMore() {
-    console.log("readMode this", this)
-    // this.setState({displayBio: true});
+  toggleDisplayBio() {
+    this.setState({displayBio: !this.state.displayBio});
   }
 
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Alice. I'm a software engineer.</p>
+        <p>I'm always looking forward to learning interesting and different technologies.</p>
+        {
+          this.state.displayBio ? (
             <div>
-              <h1>Hello!</h1>
-              <p>My name is Alice. I'm a software engineer.</p>
-              <p>I'm always looking forward to learning interesting and different technologies.</p>
-              {
-                this.state.displayBio ? (
-                  <div>
-                    <p>I live in São Paulo, and code every day.</p>
-                    <p>My favorite language is Python, and I love .NET and JavaScript too :)</p>
-                    <p>Besides coding, I also love games and anime. My favourite game is Final Fantasy, and my favourite anime is One Piece.</p>
-                  </div>
-                ) : (
-                  <div>
-                    <button onClick={this.readMore}>Read more</button>
-                  </div>
-                )
-              }
+              <p>I live in São Paulo, and code every day.</p>
+              <p>My favorite language is Python, and I love .NET and JavaScript too :)</p>
+              <p>Besides coding, I also love games and anime. My favourite game is Final Fantasy, and my favourite anime is One Piece.</p>
+              <button onClick={this.toggleDisplayBio}>Show less</button>
             </div>
-        )
-    }
+          ) : (
+            <div>
+              <button onClick={this.toggleDisplayBio}>Read more</button>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+
 }
 
 export default App;
